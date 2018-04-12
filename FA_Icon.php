23008901b143840770ac7460e958d3501d91b22a
gizmore <gizmore@wechall.net>
2018-04-12 23:51:41 +0200

    6.07_fontawesome

--- FA_Icon.php
+++ FA_Icon.php
@@ -8,45 +8,62 @@ use GDO\UI\GDT_IconUTF8;
  * 
  * @author gizmore
  * @since 6.01
- * @version 6.05
+ * @version 6.07
  */
 final class FA_Icon
 {
-	public static function iconS($icon, $style)
+	public static function iconS($icon, $iconText, $style)
 	{
 		static $map = array(
 			'add' => 'plus',
+			'alert' => 'bell',
+			'all' => 'list',
+			'arrow_down' => 'sort-down',
+			'arrow_left' => 'arrow-left',
+			'arrow_right' => 'arrow-right',
+			'arrow_up' => 'sort-up',
 			'create' => 'plus-circle',
 			'delete' => 'minus-circle',
+			'download' => 'download',
+			'edit' => 'edit',
+			'face' => 'user',
+			'help' => 'question-circle',
+			'level' => 'trophy',
+			'list' => 'th-list',
+			'quote' => 'quote-left',
 			'remove' => 'minus',
-			'up' => 'caret-up',
-			'down' => 'caret-down',
-			'left' => 'chevron-left',
-			'right' => 'chevron-right',
-			'done' => 'check',
-			'success' => 'check-square',
-			'error' => 'exclamation-triangle',
-			'email' => 'envelope-o',
-			'title' => 'header',
-			'message' => 'paragraph',
-			'captcha' => 'lock',
-			'tooltip' => 'info-circle',
-			'star' => 'star',
-			'tags' => 'tags',
-			'file' => 'paperclip',
-			'settings' => 'gear',
-			'refresh' => 'refresh',
-			'credits' => 'money',
-			'back' => 'undo',
-			'link' => 'chain',
-			'upload' => 'upload',
+			'reply' => 'reply',
+			'search' => 'search',
+			'view' => 'eye',
 			
+// // 			'up' => 'caret-up',
+// // 			'down' => 'caret-down',
+// // 			'left' => 'chevron-left',
+// // 			'right' => 'chevron-right',
+// 			'done' => 'check',
+// 			'success' => 'check-square',
+// 			'error' => 'exclamation-triangle',
+// 			'email' => 'envelope-o',
+// 			'title' => 'header',
+// 			'message' => 'paragraph',
+// 			'captcha' => 'lock',
+// 			'tooltip' => 'info-circle',
+// 			'star' => 'star',
+// 			'tags' => 'tags',
+// 			'file' => 'paperclip',
+// 			'settings' => 'gear',
+// 			'refresh' => 'refresh',
+// 			'credits' => 'money',
+// 			'back' => 'undo',
+// 			'link' => 'chain',
+// 			'upload' => 'upload',
 		);
 		if (!isset($map[$icon]))
 		{
-			return GDT_IconUTF8::iconS($icon, $style);
+			return GDT_IconUTF8::iconS($icon, $iconText, $style);
+// 			return "XX $icon XX" . GDT_IconUTF8::iconS($icon, $iconText, $style);
 		}
-		return sprintf('<span class="gdo-icon"%s><i class="fa fa-%s"></i></span>', $style, $map[$icon]);
+		return sprintf('<span class="gdo-icon"%s><i class="fa fa-%s" title="%s"></i></span>', $style, $map[$icon], $iconText);
 	}
 	
 }
